978ec6f82e05fa98de6c16ab7a8139ea4f1eb835
warjey <salundaga.365191@sjdelmonte.sti.edu.ph>
2026-02-27 18:39:52 +0800

    modified asbtract classes and command runner

--- command_runner/lib/command_runner.dart
+++ command_runner/lib/command_runner.dart
@@ -1,7 +1,7 @@
-/// A simple command runner to handle command-line arguments.
-///
-/// More extensive documentation for this library goes here.
+
 library;
 
+export 'src/arguments.dart';
 export 'src/command_runner_base.dart';
-// TODO: Export any other libraries intended for clients of this package.
+export 'src/help_command.dart';
+
